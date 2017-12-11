2b043db4ee230a372d15eec507ab655efc0859b4
Colin Nolan <colin.nolan@sanger.ac.uk>
2017-12-11 15:14:35 +0000

    Creates storage handlers.

---
+++ thriftybuilder/storage.py
@@ -0,0 +1,61 @@
+import json
+from abc import ABCMeta, abstractmethod
+from typing import Optional, Dict
+
+import os
+
+
+class Storage(metaclass=ABCMeta):
+    """
+    TODO
+    """
+    @abstractmethod
+    def get_checksum(self, configuration_id: str) -> Optional[str]:
+        """
+        TODO
+        :param configuration_id:
+        :return:
+        """
+
+    @abstractmethod
+    def set_checksum(self, configuration_id: str, checksum: str):
+        """
+        TODO
+        :param configuration_id:
+        :param checksum:
+        :return:
+        """
+
+
+class MemoryStorage(Storage):
+    """
+    TODO
+    """
+    def __init__(self):
+        self._data: Dict[str, str] = {}
+
+    def get_checksum(self, configuration_id: str) -> Optional[str]:
+        return self._data.get(configuration_id, None)
+
+    def set_checksum(self, configuration_id: str, checksum: str):
+        self._data[configuration_id] = checksum
+
+
+class DiskStorage(Storage):
+    """
+    TODO
+
+    This storage was created to quickly get persistence - concurrent access is unsafe!
+    """
+    def __init__(self, storage_file_location: str):
+        self.storage_file_location = storage_file_location
+
+    def get_checksum(self, configuration_id: str) -> Optional[str]:
+        with open(self.storage_file_location, "r") as file:
+            return json.loads(file).get(configuration_id, None)
+
+    def set_checksum(self, configuration_id: str, checksum: str):
+        with open(self.storage_file_location, "rw") as file:
+            configuration = json.loads(file)
+            configuration[configuration_id] = checksum
+            file.write(json.dumps(configuration))
