e9c9a3a7a4611ddf116f256074e0b3f1c0ed04c4
Colin Nolan <colin.nolan@sanger.ac.uk>
2018-03-20 14:55:31 +0000

    Releases version 1.0.1.

--- thriftybuilder/meta.py
+++ thriftybuilder/meta.py
@@ -1,4 +1,4 @@
 PACKAGE_NAME = "thriftybuilder"
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 DESCRIPTION = "Builds Docker images, capturing information to reduce the frequency of future re-builds"
 EXECUTABLE_NAME = "thrifty"
