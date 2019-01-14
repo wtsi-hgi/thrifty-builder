0a48bee991ad31456d5bc931f8aaee4b1fcb72b7
Joshua C. Randall <jcrandall@alum.mit.edu>
2019-01-14 15:28:43 +0000

    fix indentation error in merge of with

--- thriftybuilder/cli.py
+++ thriftybuilder/cli.py
@@ -119,8 +119,8 @@ def main(cli_arguments: List[str], stdin_content: Optional[str]=None):
     else:
         for repository in configuration.docker_registries:
             with DockerUploader(configuration.checksum_storage, repository) as uploader:
-            for build_configuration in build_configurations_to_upload:
-                uploader.upload(build_configuration)
+                for build_configuration in build_configurations_to_upload:
+                    uploader.upload(build_configuration)
 
     all_built: Dict[str, str] = {}
     built_now: Dict[str, str] = {}
